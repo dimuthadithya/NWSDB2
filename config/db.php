b8b3a0a0335f9a06a8672fdb71e94d5c0093ea7f
dimuthadithya <dimuthx353@gmail.com>
2025-12-11 15:30:32 +0530

    feat: add initial database configuration file with local connection details

--- config/db.php
+++ config/db.php
@@ -1,11 +1,24 @@
 <?php
 // Database configuration
 // define('DB_HOST', 'db.fr-pari1.bengt.wasmernet.com:10272');
-// define('DB_USERNAME', 'c28da5dd73f8800068539d9dca6f');
-// define('DB_PASSWORD', '0691c28d-a5dd-755b-8000-c3c4c12a768b');
-// define('DB_NAME', 'devicerecordsystem4');
+// define('DB_USERNAME', 'a8eebd897f628000db2acf629bb8');
+// define('DB_PASSWORD', '0693a8ee-bd8a-7133-8000-58f9419aae65');
+// define('DB_NAME', 'devicerecordsystem6');
 
 define('DB_HOST', 'localhost');
 define('DB_USERNAME', 'root');
 define('DB_PASSWORD', '');
 define('DB_NAME', 'devicerecordsystem');
+
+
+// Host: 
+// db.fr-pari1.bengt.wasmernet.com
+// Port: 
+// 10272
+// Name: 
+// devicerecordsystem6
+// Username: 
+// a8eebd897f628000db2acf629bb8
+// Password: 
+
+// 0693a8ee-bd8a-7133-8000-58f9419aae65
